c1d90ec6559d4ecdb8175b3fb0573e44cdef2e2d
nyeholt <nyeholt@gmail.com>
2010-06-10 17:08:33 +1000

    Add sample test

---
+++ mysite/tests/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+/* All code covered by the BSD license located at http://silverstripe.org/bsd-license/ */
+
+/**
+ * @author Marcus Nyeholt <marcus@silverstripe.com.au>
+ */
+class SsauSampleTest extends SapphireTest {
+    public function testSomething() {
+		$this->assertTrue(true);
+	}
+}
