25a627b32c50fdaa0d67d1518d9202f617238fa1
nyeholt <nyeholt@gmail.com>
2010-06-16 14:32:54 +1000

    remove obsolete files

--- ssautesting/testing.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $TESTING_CONFIG;
-$TESTING_CONFIG = array(
-        "database" => "tmpdb_ssautesting",
-		"reporter" => "PhingXMLPHPUnit3ResultFormatter",
-		"logfile" => "ssautesting/logs/testsuite.xml"
-);
