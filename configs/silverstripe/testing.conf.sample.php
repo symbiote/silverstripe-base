af0a60c84db56f3a7b0b07ef1087e25e7760c80b
nyeholt <nyeholt@gmail.com>
2010-10-20 16:18:25 +0700

    MINOR: Make sure to never exclude .svn dirs for certain package calls. Remove config folders

--- configs/silverstripe/testing.conf.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $TESTING_CONFIG;
-$TESTING_CONFIG = array(
-        "database" => "${testing.db.name}",
-		"reporter" => "${testing.reporter}",
-		"logfile" => "${testing.logdir}/${testing.logfile}"
-);
