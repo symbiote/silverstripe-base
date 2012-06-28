8bbe1ae680fec63e1227700e177ea585b9faa32f
Marcus Nyeholt <marcus@silverstripe.com.au>
2012-06-28 11:21:52 +1000

    Updated to rename to framework, and remove obsolete testing guff

--- ssautesting/_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (file_exists(dirname(__FILE__).'/testing.conf.php')) {
-	include_once dirname(__FILE__).'/testing.conf.php';
-	
-	Director::addRules(100, array(
-		'dev' => 'ParameterisedDevelopmentController',
-	));
-}
